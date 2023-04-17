5d978920539d6f31b8eef799a84cc2dee131fdcf
sorvigolovaa <sbknugaara@gmail.com>
2023-04-17 13:07:35 +0500

    feat: add file-name

--- src/file-names.js
+++ src/file-names.js
@@ -1,23 +1,21 @@
-const { NotImplementedError } = require('../extensions/index.js');
+function renameFiles(names) {
+  const map = new Map();
+  const result = [];
 
-/**
- * There's a list of file, since two files cannot have equal names,
- * the one which comes later will have a suffix (k),
- * where k is the smallest integer such that the found name is not used yet.
- *
- * Return an array of names that will be given to the files.
- *
- * @param {Array} names
- * @return {Array}
- *
- * @example
- * For input ["file", "file", "image", "file(1)", "file"],
- * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
- *
- */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+  for (const name of names) {
+    let newName = name;
+    let count = map.get(name) || 0;
+
+    while (map.has(newName)) {
+      count++;
+      newName = `${name}(${count})`;
+    }
+
+    map.set(newName, 1);
+    result.push(newName);
+  }
+
+  return result;
 }
 
 module.exports = {
