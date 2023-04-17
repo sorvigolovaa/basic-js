33fe1c067c5bf4f5f4b19c3066ab0f20ed5682de
sorvigolovaa <sbknugaara@gmail.com>
2023-04-17 20:31:51 +0500

    fix: change season code

--- src/what-season.js
+++ src/what-season.js
@@ -1,43 +1,24 @@
 function getSeason(date) {
-  if (!date) {
-    return 'Unable to determine the time of year!';
-  }
-  
-  if (!(date instanceof Date)) {
+  if (!date || Object.prototype.toString.call(date) !== '[object Date]' || isNaN(date.getTime())) {
     throw new Error('Invalid date!');
   }
-  
-  if (isNaN(date.getTime())) {
-    throw new Error('Invalid date!');
-  }
-  
-  if (Object.prototype.toString.call(date) !== "[object Date]") {
-    throw new Error('Invalid date!');
-  }
-  
+
   const month = date.getMonth();
-  
-  switch (month) {
-    case 11:
-    case 0:
-    case 1:
-      return 'winter';
-    case 2:
-    case 3:
-    case 4:
-      return 'spring';
-    case 5:
-    case 6:
-    case 7:
-      return 'summer';
-    case 8:
-    case 9:
-    case 10:
-      return 'fall';
-    default:
-      throw new Error('Invalid date!');
+  if (month < 2 || month === 11) {
+    return 'winter';
+  } else if (month < 5) {
+    return 'spring';
+  } else if (month < 8) {
+    return 'summer';
+  } else {
+    return 'fall';
   }
 }
+
+module.exports = {
+  getSeason
+};
+
 
 
 
