83937990f0b0f0fdb488a9714116b3e069af7096
sorvigolovaa <sbknugaara@gmail.com>
2023-04-17 12:32:10 +0500

    feat: add season code

--- src/what-season.js
+++ src/what-season.js
@@ -1,19 +1,36 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const moment = require('moment');
 
-/**
- * Extract season from given date and expose the enemy scout!
- * 
- * @param {Date | FakeDate} date real or fake date
- * @returns {String} time of the year
- * 
- * @example
- * 
- * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
- */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  if (!date) {
+    return 'Unable to determine the time of year!';
+  }
+
+  if (!moment(date).isValid()) {
+    throw new Error('Invalid date!');
+  }
+
+  const month = moment(date).month();
+
+  switch (month) {
+    case 11:
+    case 0:
+    case 1:
+      return 'winter';
+    case 2:
+    case 3:
+    case 4:
+      return 'spring';
+    case 5:
+    case 6:
+    case 7:
+      return 'summer';
+    case 8:
+    case 9:
+    case 10:
+      return 'fall';
+    default:
+      throw new Error('Invalid month!');
+  }
 }
 
 module.exports = {
