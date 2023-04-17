4cb6906a216d8771d96b39510d121053dc220858
sorvigolovaa <sbknugaara@gmail.com>
2023-04-17 10:43:30 +0500

    feat: add count example

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,22 +1,13 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
-/**
- * Given matrix where you have to find cats by ears "^^"
- *
- * @param {Array<Array>} matrix 
- * @return {Number} count of cats found
- *
- * @example
- * countCats([
- *  [0, 1, '^^'],
- *  [0, '^^', 2],
- *  ['^^', 1, 2]
- * ]) => 3`
- *
- */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(matrix) {
+  let count = 0;
+  matrix.forEach(row => {
+    row.forEach(element => {
+      if (element === '^^') {
+        count++;
+      }
+    });
+  });
+  return count;
 }
 
 module.exports = {
